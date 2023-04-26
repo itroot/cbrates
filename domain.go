85ebe4eeda4f768d3e1bdcadde371e1f7533fba2
Ivan Tolstosheyev <ivan.tolstosheyev@gmail.com>
2023-04-26 18:12:55 +0300

    Add some more comments

--- domain.go
+++ domain.go
@@ -1,3 +1,4 @@
+// Package cbrates implements client for the API of Central Bank of Russia, described here: http://www.cbr.ru/development/sxml/ .
 package cbrates
 
 import (
@@ -49,6 +50,7 @@ type Valute struct {
 	Value    string  `xml:"Value"`
 }
 
+// MustParseFloat64 returns currency value in RUB in float64.
 func (v *Valute) MustParseFloat64() float64 {
 	value, err := strconv.ParseFloat(strings.Join(strings.Split(v.Value, ","), "."), 64)
 	if err != nil {
@@ -57,17 +59,20 @@ func (v *Valute) MustParseFloat64() float64 {
 	return value
 }
 
+// Client is a interface for accessing the CBR API.
 type Client interface {
 	// GetDailyRates implements a client to the http://www.cbr.ru/development/sxml/ for a single day request
 	GetDailyRates(ctx context.Context, year int, month time.Month, day int) (*ValCurs, error)
 }
 
+// NewClient creates a new client for the CBR API.
 func NewClient(logger *log.Logger) (Client, error) {
 	return &client{
 		logger: logger,
 	}, nil
 }
 
+// NewClient creates cache wrapper and a new client for the CBR API.
 func NewCachedClient(logger *log.Logger) (Client, error) {
 	cachePath := configdir.LocalCache("cbrates")
 	if err := configdir.MakePath(cachePath); err != nil {
